ebb30e1342ded0bfcb48a84a05fa8a267948b443
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-24 09:23:14 +0500

    🔥 Code Update 🔥

--- lib/pages/counter_page/counter_page.dart
+++ lib/pages/counter_page/counter_page.dart
@@ -5,6 +5,21 @@ class CounterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('COUNTER'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          0.toString(),
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
