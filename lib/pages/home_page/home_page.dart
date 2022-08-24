33209e48d6ce269fb95a7e5c22b244ef2cb28022
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-24 09:18:54 +0500

    🔥 Code Update 🔥

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_learning/pages/counter_page/counter_page.dart';
 
 // * If we want to provide a string throughout the app using riverpod
 // * Keep in mind! The object being provided by the provider is called state
@@ -15,13 +16,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('HOME'),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Consumer(
-          builder: (context, watch, child){
-            final number = watch;
-            return Text(number.toString());
+        child: ElevatedButton(
+          child: const Text('COUNTER PAGE'),
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: ((BuildContext context) => const CounterPage())));
           },
-        ),
+        )
       ),
     );
   }
